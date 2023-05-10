d4c1f37b46dc9567745af66a905e5534b401ade0
Dmitry Velykyi <rekssobaka@gmail.com>
2023-05-10 20:52:27 +0300

    add HW

--- src/script.js
+++ src/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// @param {number} fatherAge
+// @param {number} sonAge
+// @returns {number} количество лет через сколько отец будет старше
+
+function calculateAge(ageFather, ageSon) {
+    if (Math.abs(ageFather - ageSon) < 15){
+        return -1;
+    }
+const years = Math.abs(ageSon - ageFather);
+    return years;
+};
+const ageFather = 30;
+const ageSon = 20;
+const ageDifference = calculateAge(ageFather, ageSon);
+
+if (ageDifference === -1) {
+    console.log("Разница в возрасте меньше 15 лет.");
+  } else {
+    console.log(`Отцу потребуется ${ageDifference} лет, чтобы стать вдвое старше сына.`);
+  }
