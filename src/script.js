5f4882053d18d2fe196e80ebbbe8a79c4580d325
Dmitry <rekssobaka@gmail.com>
2023-05-23 20:57:28 +0300

    reworkhw

--- src/script.js
+++ src/script.js
@@ -4,22 +4,11 @@
 // @param {number} sonAge
 // @returns {number} количество лет через сколько отец будет старше
 
-function calculateAge(fatherAge, sonAge) {
-    if (sonAge <= 0 || fatherAge <= 0 || fatherAge - sonAge < 15) {
-      return -1;
-    }
-    let years = 0;
-    while ((sonAge + years) * 2 < fatherAge + years) {
-      years++;
-    }
-    return years;
+const ageTwoFold = (sonAge, fatherAge) => {
+  let result = 0;
+  if (fatherAge - sonAge < 15 || sonAge < 0) {
+    return 'You entered wrong age.';
   }
-  const ageFather = 50;
-  const ageSon = 20;
-  const ageDifference = calculateAge(ageFather, ageSon);
-  
-  if (ageDifference === -1) {
-    console.log("Некорректный возраст.");
-  } else {
-    console.log(`Через ${ageDifference} лет, отец будет вдвое старше сына.`);
-  }
+  return fatherAge -2 * sonAge;
+}
+console.log(ageTwoFold(30,65));
